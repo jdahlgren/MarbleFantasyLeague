2554a9157aab36a985053be86dec3ad536c0e000
Johannes <johannes.dahlgren@gmail.com>
2022-03-13 16:50:33 +0100

    Init repo

---
+++ src/main/java/se/johannesdahlgren/marble/highscore/user/MarbleHighScoreUserServiceApplication.java
@@ -0,0 +1,13 @@
+package se.johannesdahlgren.marble.highscore.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarbleHighScoreUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarbleHighScoreUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/johannesdahlgren/marble/highscore/user/MarbleHighScoreUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.johannesdahlgren.marble.highscore.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarbleHighScoreUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
