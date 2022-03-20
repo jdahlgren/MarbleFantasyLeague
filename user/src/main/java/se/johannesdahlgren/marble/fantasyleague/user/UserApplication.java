45815e2ec35d4f9ca1f9ff4fe8b31455af3910bb
Johannes <johannes.dahlgren@gmail.com>
2022-03-20 19:42:48 +0100

    move user to sub-module

--- user/src/main/java/se/johannesdahlgren/marble/fantasyleague/user/UserApplication.java
+++ user/src/main/java/se/johannesdahlgren/marble/fantasyleague/user/UserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarbleFantasyLeagueApplication {
+public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MarbleFantasyLeagueApplication.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 
 }
